93498b946fe8cb53df8f817d45f9d3c7d5ef31f4
rithviknishad <mail@rithviknishad.dev>
2024-08-19 23:30:58 +0530

    Adds scripts for migrating milestone labels to milestones

---
+++ gh-issues-to-milestones/main.ts
@@ -0,0 +1,62 @@
+import { Octokit } from "@octokit/rest";
+import { exit } from "process";
+
+const paginate = <T>(items: readonly T[], batchSize: number) => {
+  const result: T[][] = [];
+  const _items = [...items];
+  while (_items.length) {
+    result.push(_items.splice(0, batchSize));
+  }
+  return result;
+};
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN!,
+});
+
+const [owner, repo] = process.env.REPO!.split("/");
+const milestone = parseInt(process.env.MILESTONE!);
+const labels = process.env.LABELS!;
+
+async function getIssues() {
+  const issues = await octokit.issues.list({
+    owner,
+    repo,
+    labels,
+    state: "all",
+  });
+
+  if (!issues.data.length) {
+    console.error("No issues found with the specified label.");
+    exit(1);
+  }
+
+  return issues.data.map((issue) => issue.number);
+}
+
+async function main() {
+  const issues = await getIssues();
+  console.info(`Found ${issues.length} issues with labels "${labels}".`);
+
+  for (const page of paginate(issues, 10)) {
+    console.info(
+      `Batch updating ${page.length} issues: (${page
+        .map((i) => `#${i}`)
+        .join(", ")})`
+    );
+
+    await Promise.all(
+      page.map((issue_number) =>
+        octokit.issues.update({
+          owner,
+          repo,
+          issue_number,
+          milestone,
+        })
+      )
+    );
+  }
+  console.info("Updated.");
+}
+
+main();
